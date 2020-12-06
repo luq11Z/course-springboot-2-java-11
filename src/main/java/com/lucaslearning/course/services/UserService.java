6352b709edf352c7f5371611ec7bb7dc54ee271e
Lucas Silva <lucas_segundinho@hotmail.com>
2020-12-06 16:15:02 +0000

    Exception handling - update

--- src/main/java/com/lucaslearning/course/services/UserService.java
+++ src/main/java/com/lucaslearning/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.lucaslearning.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -20,41 +22,46 @@ import com.lucaslearning.course.services.exceptions.ResourceNotFoundException;
  * */
 @Service
 public class UserService {
-	
-	//injecao de dependencia do UserRepository
+
+	// injecao de dependencia do UserRepository
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id); //essa operacao retorna um obj do tipo Optional
-		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); //retorna o obj do tipo User dentro do Optional
+		Optional<User> obj = repository.findById(id); // essa operacao retorna um obj do tipo Optional
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // retorna o obj do tipo User dentro do
+																			// Optional
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
-		repository.deleteById(id);
+			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-	
-	//atualiza o entity com base no obj
+
+	// atualiza o entity com base no obj
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
