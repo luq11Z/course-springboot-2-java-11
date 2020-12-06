8b4b6f94233403dbcb3b7eb490641b5c10d15465
Lucas Silva <lucas_segundinho@hotmail.com>
2020-12-06 15:58:29 +0000

    Exception handling - delete

--- src/main/java/com/lucaslearning/course/services/UserService.java
+++ src/main/java/com/lucaslearning/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.lucaslearning.course.entities.User;
 import com.lucaslearning.course.repositories.UserRepository;
+import com.lucaslearning.course.services.exceptions.DatabaseException;
 import com.lucaslearning.course.services.exceptions.ResourceNotFoundException;
 
 /* component registration, quando um objeto vai poder ser injetavel pelo mecanismo de injecao
@@ -36,7 +39,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
